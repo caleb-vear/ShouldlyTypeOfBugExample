b8e46b4e5bc747a8c527488f9f09d3000d3fe9d9
caleb-vear <caleb.vear@gmail.com>
2011-08-24 08:58:07 +1000

    Initial commit

---
+++ ShouldlyTypeOfBugExample/ShouldlyTypeOfTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Shouldly;
+
+namespace ShouldlyTypeOfBugExample
+{
+    [TestFixture]
+    public class ShouldlyTypeOfTests
+    {
+        [Test]
+        public void ShouldlyMessage_CanGenerateForOfTypeAssertion()
+        {
+            Should.Throw<Exception>(() => 2.ShouldBeTypeOf<double>())
+                .Message.ShouldBe("() => 2 should be type of System.Double but was System.Int32");
+        }
+    }
+}
